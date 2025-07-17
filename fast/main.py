9b34b2104f3fc00fd565d34f9cc8259f50f978e1
bvalmeida <bvalmeida94@gmail.com>
2025-07-17 12:20:25 -0300

    iniciando o fastapi

---
+++ fast/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+]
+
+
+@app.get('/')
+async def index() -> dict[str, str]:
+    return {'hello': 'world'}
+
+@app.get('/about')
+async def about() -> str:
+    return 'An Exceptional Company'
